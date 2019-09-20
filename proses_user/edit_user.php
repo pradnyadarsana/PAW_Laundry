c29e8de93941d100266cb31b7995e288d38adc55
michaeljohandy <michaeljo2511@gmail.com>
2019-09-20 18:20:06 +0700

    edit_paket.php , edit_akun.php, edit_paket.php

--- proses_user/edit_user.php
+++ proses_user/edit_user.php
@@ -1,8 +1,8 @@
 <?php
-if(isset($_POST['store'])){
+if(isset($_POST['store'])&& ! empty($_POST['id'])){
     include('../db.php');
     $id = $_POST['id'];
-    $name = $_POST['name'];
+    $nama = $_POST['nama'];
     $telp = $_POST['telp'];      
     $email = $_POST['email']; 
     $username = $_POST['username'];
@@ -11,11 +11,11 @@ if(isset($_POST['store'])){
     $jenisKelamin = $_POST['jenis_kelamin'];
    
 
-$edit=mysqli_query($con,"UPDATE data_pengguna SET name='$name',telp='$telp', ,email='$email',username-'$username',password='$password',tanggalLahir='$tangga_lLahir',jenisKelamin='$jenis_kelamin'  WHERE id='$id'")or die(mysqli_error($con));
+$edit=mysqli_query($con,"UPDATE data_pengguna SET nama='$nama',telp='$telp', ,email='$email',username-'$username',password='$password',tanggalLahir='$tangga_lLahir',jenisKelamin='$jenis_kelamin'  WHERE id='$id'")or die(mysqli_error($con));
 if($edit){
-    echo '<script>alert("Berhasil Mengubah"); window.location = ".."</script>';
+    echo '<script>alert("Berhasil Mengubah"); window.location = "../us_dashboard/edit_user.php"</script>';
 }else{
-    echo '<script>alert("Terjadi Kesalahan"); window.location = ".."</script>';
+    echo '<script>alert("Terjadi Kesalahan"); window.location = "../us_dashboard/lihat_akun.php"</script>';
 }
 }else{
     echo '<script>window.history.back()</script>';
