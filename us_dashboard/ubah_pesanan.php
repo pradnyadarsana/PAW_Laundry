600c5cd449feac080135ce344807d5b277f9021b
arnandobs <arnandobs20@gmail.com>
2019-09-25 17:20:13 +0700

    udah php

--- us_dashboard/ubah_pesanan.php
+++ us_dashboard/ubah_pesanan.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+if($_SESSION['isLogin']){
+    include "../us_layout/us_dashboard.php";
+    $user=$_SESSION['user'];
+
+    function active_radio_button($value,$input){
+        $result =  $value==$input?'checked':'';
+        return $result;
+    }
+
+echo'
 <!doctype html>
 <html lang="en">
     <head>
@@ -8,7 +20,6 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="css/style.css">
-        <link rel="icon" href="img/icon/logo.jpg">
     </head>
     <body id="top" class="join-page">
         <title>Tambah Pesanan - PAW Laundry</title>
@@ -16,7 +27,7 @@
         <div class="jumbotron full-transparent mb-0">
             <div class="container">
                 <div class="mt-5 text-center">
-                    <h1 class="display-4">Tambah Pesanan</h1>
+                    <h1 class="display-4">Ubah Pesanan</h1>
                 </div>
                 <hr class="my-5">
                 <div>
@@ -71,4 +82,6 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
-</html>
+</html>'
+    }
+?
