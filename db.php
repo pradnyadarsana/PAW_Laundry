320040fbb1785f365802d1f2dd6733485f70b17d
pradnyadarsana <pradnyadarsana13@gmail.com>
2019-09-26 04:22:54 +0700

    db fix

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $dbServer = "localhost";
-$username = "root";
-$password = "";
-$dbName = "pawlaundry";
+$username = "id10873022_localhost";
+$password = "123456789";
+$dbName = "id10873022_paw_laundry";
 $con = mysqli_connect($dbServer, $username, $password, $dbName);
 if(mysqli_connect_error())
 {
